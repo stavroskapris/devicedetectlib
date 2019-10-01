3514185c92a15d8a264309557e828c4dfdd6add2
stavrosKap <stavros.kapridakis@spitogatos.gr>
2019-10-01 14:45:40 +0300

    initial commit

---
+++ src/Device_Detect.php
@@ -0,0 +1,306 @@
+<?php
+
+namespace App;
+
+use Mobile_Detect;
+
+/**
+ * Class Device_Detect
+ *
+ * @package App
+ */
+class Device_Detect
+{
+    /**
+     * The User-Agent HTTP header is stored in here.
+     *
+     * @var string
+     */
+    protected $userAgent = null;
+
+    /**
+     * @var Mobile_Detect
+     */
+    protected $mobileDetect;
+
+    /**
+     * @var boolean
+     */
+    protected $isMobile;
+
+    /**
+     * List of Operating Systems.
+     *
+     * @var array
+     */
+    protected static $operatingSystems = [
+        '/windows nt 10/i'      => 'Windows 10',
+        '/windows nt 6.3/i'     => 'Windows 8.1',
+        '/windows nt 6.2/i'     => 'Windows 8',
+        '/windows nt 6.1/i'     => 'Windows 7',
+        '/windows nt 6.0/i'     => 'Windows Vista',
+        '/windows nt 5.2/i'     => 'Windows Server 2003/XP x64',
+        '/windows nt 5.1/i'     => 'Windows XP',
+        '/windows xp/i'         => 'Windows XP',
+        '/windows nt 5.0/i'     => 'Windows 2000',
+        '/windows me/i'         => 'Windows ME',
+        '/win98/i'              => 'Windows 98',
+        '/win95/i'              => 'Windows 95',
+        '/win16/i'              => 'Windows 3.11',
+        '/macintosh|mac os x/i' => 'Mac OS X',
+        '/mac_powerpc/i'        => 'Mac OS 9',
+        '/linux/i'              => 'Linux',
+        '/ubuntu/i'             => 'Ubuntu',
+        '/cros/i'               => 'Chrome OS',
+        '/iphone/i'             => 'iPhone',
+        '/ipod/i'               => 'iPod',
+        '/ipad/i'               => 'iPad',
+        '/android/i'            => 'Android',
+        '/blackberry/i'         => 'BlackBerry',
+        '/BB/i'                 => 'BlackBerry',
+        '/webos/i'              => 'Mobile'
+    ];
+
+    /**
+     * List of Browsers
+     *
+     * @var array
+     */
+    protected static $browsers = [
+        '/msie/i'                                                                 => 'Internet Explorer',
+        '/trident/i'                                                              => 'Internet Explorer',
+        '/firefox/i'                                                              => 'Firefox',
+        '/safari/i'                                                               => 'Safari',
+        '/chrome/i'                                                               => 'Chrome',
+        '/edge/i'                                                                 => 'Edge',
+        '/opera/i'                                                                => 'Opera',
+        '/opr/i'                                                                  => 'Opera',
+        '/netscape/i'                                                             => 'Netscape',
+        '/maxthon/i'                                                              => 'Maxthon',
+        '/konqueror/i'                                                            => 'Konqueror',
+        '/Skyfire/i'                                                              => 'Skyfire',
+        //mobile browsers
+        '/Version.*Mobile.*Safari|Safari.*Mobile|MobileSafari/i'                  => 'Safari',
+        '/bCrMo|CriOS|Android.*Chrome|Mobile.*Chrome/i'                           => 'Chrome',
+        '/Opera.*Mini|Opera.*Mobi|Android.*Opera|Mobile.*OPR/i'                   => 'Opera',
+        '/Mobile SafariEdge/i'                                                    => 'Edge',
+        '/IEMobile|MSIEMobile/i'                                                  => 'Internet Explorer',
+        '/fennec|firefox.*maemo|(Mobile|Tablet).*Firefox|Firefox.*Mobile|FxiOS/i' => 'Firefox',
+    ];
+
+    /**
+     * Device types
+     *
+     * @var array
+     */
+    protected static $deviceTypes = ['mobile', 'tablet', 'desktop'];
+
+    /**
+     * Device_Detect constructor.
+     *
+     * @param Mobile_Detect $mobileDetect
+     * @param string $userAgent Inject the User-Agent header. If null, will use HTTP_USER_AGENT
+     *                          from the $_SERVER array instead.
+     */
+    public function __construct(Mobile_Detect $mobileDetect, $userAgent = null)
+    {
+        $this->setUserAgent($userAgent);
+        $this->mobileDetect = $mobileDetect;
+    }
+
+    /**
+     * Set the User-Agent to be used.
+     *
+     * @param string $userAgent
+     * @return void
+     */
+    public function setUserAgent($userAgent = null): void
+    {
+        if (!empty($userAgent)) {
+            $this->userAgent = $this->prepareUserAgent($userAgent);
+        } else {
+            $userAgent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
+            $this->userAgent = $this->prepareUserAgent($userAgent);
+        }
+    }
+
+    /**
+     * Retrieve the User-Agent.
+     *
+     * @return string|null The user agent if it's set.
+     */
+    public function getUserAgent(): ?string
+    {
+        return $this->userAgent;
+    }
+
+    /**
+     * @param string $userAgent
+     * @return string
+     */
+    private function prepareUserAgent($userAgent): string
+    {
+        $userAgent = trim($userAgent);
+        $userAgent = substr($userAgent, 0, 500);
+
+        return $userAgent;
+    }
+
+    /**
+     * Retrieve the list of known browsers.
+     *
+     * @return array
+     */
+    public function getBrowsers(): array
+    {
+        return self::$browsers;
+    }
+
+    /**
+     * Retrieve the list of operating systems.
+     *
+     * @return array
+     */
+    public function getOperatingSystems(): array
+    {
+        return self::$operatingSystems;
+    }
+
+    /**
+     * Retrieve the list of device types.
+     *
+     * @return array
+     */
+    public function getDeviceTypes(): array
+    {
+        return self::$deviceTypes;
+    }
+
+    /**
+     * @param null $userAgent
+     * @return string
+     */
+    public function getDeviceType($userAgent = null): string
+    {
+        if ($this->isComputer($userAgent)) {
+            return 'desktop';
+        } elseif ($this->isTablet($userAgent)) {
+            return 'tablet';
+        } else {
+            return 'mobile';
+        }
+    }
+
+    /**
+     * Get device browser
+     *
+     * @return string
+     */
+    public function getBrowser(): string
+    {
+        $browser = "Unknown Browser";
+        $browser_array = $this->getBrowsers();
+        foreach ($browser_array as $regex => $value) {
+            if (preg_match($regex, $this->userAgent)) {
+                $browser = $value;
+            }
+        }
+
+        return $browser;
+    }
+
+    /**
+     * Get device OS Platform
+     *
+     * @return string
+     */
+    public function getOperatingSystem(): string
+    {
+        $os_platform = "Unknown OS Platform";
+        $os_array = $this->getOperatingSystems();
+        foreach ($os_array as $regex => $value) {
+            if (preg_match($regex, $this->userAgent)) {
+                $os_platform = $value;
+            }
+        }
+
+        return $os_platform;
+    }
+
+    /**
+     * Mobile device detection
+     *
+     * @param null $userAgent
+     * @return bool
+     */
+    public function isMobile($userAgent = null): bool
+    {
+        return $this->mobileDetect->isMobile($userAgent);
+    }
+
+    /**
+     * Tablet detection
+     *
+     * @param null $userAgent
+     * @return bool
+     */
+    public function isTablet($userAgent = null): bool
+    {
+        return $this->mobileDetect->isTablet($userAgent);
+    }
+
+    /**
+     * Computer detection
+     * Since it is not a mobile device, probably
+     * it is safe to assume that the device is computer
+     *
+     * @param null $userAgent
+     * @return bool
+     */
+    public function isComputer($userAgent = null): bool
+    {
+        return (!$this->mobileDetect->isMobile($userAgent) && !$this->mobileDetect->isTablet($userAgent));
+    }
+
+    /**
+     * Ipad detection
+     *
+     * @return bool
+     */
+    public function isIpad(): bool
+    {
+        return $this->mobileDetect->is('Ipad');
+    }
+
+    /**
+     * Iphone detection
+     *
+     * @return bool
+     */
+    public function isIphone(): bool
+    {
+        return $this->mobileDetect->is('iphone');
+    }
+
+    /**
+     * Android Os detection
+     *
+     * @return bool
+     */
+    public function isAndroid(): bool
+    {
+        /** @noinspection PhpUndefinedMethodInspection */
+        return $this->mobileDetect->isAndroidOS();
+    }
+
+    /**
+     * Get version of os, browser etc
+     *
+     * @param string $key
+     * @return float|string
+     */
+    public function getVersion(string $key)
+    {
+        return $this->mobileDetect->version($key);
+    }
+}
---
+++ tests/Unit/Device_DetectTest.php
@@ -0,0 +1,296 @@
+<?php
+
+namespace Tests\Unit\Libraries;
+
+use App\Device_Detect;
+use Mobile_Detect;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class Device_DetectTest
+ *
+ * @package Tests\Unit\Libraries
+ */
+class Device_DetectTest extends TestCase
+{
+    /**
+     * @var Mobile_Detect|Mockery\LegacyMockInterface|Mockery\MockInterface
+     */
+    private $mockMobile_Detect;
+
+    /**
+     * @var mixed
+     */
+    private $deviceDetect;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->mockMobile_Detect = Mockery::mock(Mobile_Detect::class);
+        $this->deviceDetect = new Device_Detect($this->mockMobile_Detect);
+    }
+
+    /**
+     * @test
+     *
+     * @see Device_Detect::getUserAgent()
+     */
+    public function getUserAgentTest()
+    {
+        $this->deviceDetect->setUserAgent('this is a user agent string');
+
+        $this->assertEquals('this is a user agent string', $this->deviceDetect->getUserAgent());
+    }
+
+    /**
+     * @test
+     * @see Device_Detect::isMobile()
+     */
+    public function isMobileWithArgsTest()
+    {
+        $this->mockMobile_Detect->shouldReceive('isMobile')
+            ->withArgs(['this is a user agent string'])
+            ->once()
+            ->andReturn(true)
+            ->getMock();
+        $response = $this->deviceDetect->isMobile('this is a user agent string');
+        $this->assertIsBool($response);
+        $this->assertTrue($response);
+    }
+
+    /**
+     * @test
+     * @see Device_Detect::isMobile()
+     */
+    public function isMobileNoArgsTest()
+    {
+        $this->mockMobile_Detect->shouldReceive('isMobile')
+            ->withArgs([$this->deviceDetect->getUserAgent()])
+            ->once()
+            ->andReturn(true)
+            ->getMock();
+
+        $response = $this->deviceDetect->isMobile();
+        $this->assertIsBool($response);
+        $this->assertTrue($response);
+    }
+
+    /**
+     * @test
+     * @see Device_Detect::getDeviceType()
+     */
+    public function getDeskTopDeviceTypeTest()
+    {
+        $userAgentDesktop = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0';
+
+        $this->mockMobile_Detect->shouldReceive('isMobile')
+            ->withArgs([$userAgentDesktop])
+            ->once()
+            ->andReturn(false)
+            ->getMock()
+            ->shouldReceive('isTablet')
+            ->withArgs([$userAgentDesktop])
+            ->once()
+            ->andReturn(false)
+            ->getMock();
+
+        $response = $this->deviceDetect->getDeviceType($userAgentDesktop);
+        $this->assertIsString($response);
+        $this->assertEquals('desktop', $response);
+    }
+
+    /**
+     * @test
+     * @see Device_Detect::getDeviceType()
+     */
+    public function getMobileDeviceTypeTest()
+    {
+        $userAgentMobile = 'Mozilla/5.0 (Linux; Android 8.0.0; Nexus 5X Build/OPR4.170623.006) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Mobile Safari/537.36';
+
+        $this->mockMobile_Detect->shouldReceive('isMobile')
+            ->withArgs([$userAgentMobile])
+            ->once()
+            ->andReturn(true)
+            ->getMock()
+            ->shouldReceive('isTablet')
+            ->withArgs([$userAgentMobile])
+            ->once()
+            ->andReturn(false)
+            ->getMock();
+        $response = $this->deviceDetect->getDeviceType($userAgentMobile);
+        $this->assertIsString($response);
+        $this->assertEquals('mobile', $response);
+    }
+
+    /**
+     * @test
+     * @see Device_Detect::getDeviceType()
+     */
+    public function getTabletDeviceTypeTest()
+    {
+        $userAgentTablet = 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 10 Build/MOB31T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36';
+
+        $this->mockMobile_Detect->shouldReceive('isMobile')
+            ->withArgs([$userAgentTablet])
+            ->once()
+            ->andReturn(false)
+            ->getMock()
+            ->shouldReceive('isTablet')
+            ->withArgs([$userAgentTablet])
+            ->twice()
+            ->andReturn(true)
+            ->getMock();
+
+        $response = $this->deviceDetect->getDeviceType($userAgentTablet);
+        $this->assertIsString($response);
+        $this->assertEquals('tablet', $response);
+    }
+
+    /**
+     * @test
+     * @see Device_Detect::getDeviceType()
+     */
+    public function getDeviceTypeNoArgsTest()
+    {
+        $this->mockMobile_Detect->shouldReceive('isMobile')
+            ->withArgs([$this->deviceDetect->getUserAgent()])
+            ->once()
+            ->andReturn(false)
+            ->getMock()
+            ->shouldReceive('isTablet')
+            ->withArgs([$this->deviceDetect->getUserAgent()])
+            ->twice()
+            ->andReturn(true)
+            ->getMock();
+
+        $response = $this->deviceDetect->getDeviceType();
+        $this->assertIsString($response);
+        $this->assertEquals('tablet', $response);
+    }
+
+    /**
+     * @test
+     * @see Device_Detect::isMobile()
+     */
+    public function isTabletWithArgsTest()
+    {
+        $this->mockMobile_Detect->shouldReceive('isTablet')
+            ->withArgs(['this is a user agent string'])
+            ->once()
+            ->andReturn(true)
+            ->getMock();
+        $response = $this->deviceDetect->isTablet('this is a user agent string');
+        $this->assertIsBool($response);
+        $this->assertTrue($response);
+    }
+
+    /**
+     * @test
+     * @see Device_Detect::isTablet()
+     */
+    public function isTabletNoArgsTest()
+    {
+        $this->mockMobile_Detect->shouldReceive('isTablet')
+            ->withArgs([$this->deviceDetect->getUserAgent()])
+            ->once()
+            ->andReturn(true)
+            ->getMock();
+        $response = $this->deviceDetect->isTablet();
+        $this->assertIsBool($response);
+        $this->assertTrue($response);
+    }
+
+    /**
+     * @test
+     * @see Device_Detect::isComputer()
+     */
+    public function isComputerWithArgsTest()
+    {
+        $this->mockMobile_Detect->shouldReceive('isMobile')
+            ->withArgs(['this is a user agent string'])
+            ->once()
+            ->andReturn(false)
+            ->getMock()
+            ->shouldReceive('isTablet')
+            ->withArgs(['this is a user agent string'])
+            ->once()
+            ->andReturn(false)
+            ->getMock();
+        $response = $this->deviceDetect->isComputer('this is a user agent string');
+        $this->assertIsBool($response);
+        $this->assertTrue($response);
+    }
+
+    /**
+     * @test
+     * @see Device_Detect::isComputer()
+     */
+    public function isComputerNoArgsTest()
+    {
+        $this->mockMobile_Detect->shouldReceive('isMobile')
+            ->withArgs([$this->deviceDetect->getUserAgent()])
+            ->once()
+            ->andReturn(false)
+            ->getMock()
+            ->shouldReceive('isTablet')
+            ->withArgs([$this->deviceDetect->getUserAgent()])
+            ->once()
+            ->andReturn(false)
+            ->getMock();
+
+        $response = $this->deviceDetect->isComputer();
+        $this->assertIsBool($response);
+        $this->assertTrue($response);
+    }
+
+    /**
+     * @test
+     * @see Device_Detect::isIpad()
+     */
+    public function isIpadTest()
+    {
+        $this->mockMobile_Detect->shouldReceive('is')
+            ->withArgs(['Ipad'])
+            ->once()
+            ->andReturn(true)
+            ->getMock();
+        $response = $this->deviceDetect->isIpad();
+        $this->assertIsBool($response);
+        $this->assertTrue($response);
+    }
+
+    /**
+     * @test
+     * @see Device_Detect::isIphone()
+     */
+    public function isIphoneTest()
+    {
+        $this->mockMobile_Detect->shouldReceive('is')
+            ->withArgs(['iphone'])
+            ->once()
+            ->andReturn(true)
+            ->getMock();
+        $response = $this->deviceDetect->isIphone();
+        $this->assertIsBool($response);
+        $this->assertTrue($response);
+    }
+
+    /**
+     * @test
+     * @see Device_Detect::isAndroid()
+     */
+    public function isAndroidTest()
+    {
+        $this->mockMobile_Detect->shouldReceive('isAndroidOS')
+            ->withNoArgs()
+            ->once()
+            ->andReturn(true)
+            ->getMock();
+
+        $response = $this->deviceDetect->isAndroid();
+        $this->assertIsBool($response);
+        $this->assertTrue($response);
+    }
+}
